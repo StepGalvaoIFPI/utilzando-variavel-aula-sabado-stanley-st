d9501999ce36448fdc19c1acdff4f18100a66db9
Stanley Gabriel <stanley11000@gmail.com>
2025-04-16 12:07:45 -0300

    Update q10.py

--- q10.py
+++ q10.py
@@ -1,1 +1,10 @@
-
+salario = 5000
+ir = 0.1
+descontoIR = salario*ir
+inss = 0.15 
+descontoINSS = salario*inss
+valor_liqquido= salario- descontoINSS-descontoIR
+print(salario)
+print(descontoINSS)
+print(descontoIR)
+print(valor_liqquido)
